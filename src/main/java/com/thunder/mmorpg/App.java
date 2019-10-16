239fa7822451a1ec49967a46f9e91f9c0c6163cb
linkluck <linkluckstar@gmail.com>
2019-10-16 11:03:12 +0800

    new a mmorpg-agent project for hot update

---
+++ src/main/java/com/thunder/mmorpg/App.java
@@ -0,0 +1,13 @@
+package com.thunder.mmorpg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
